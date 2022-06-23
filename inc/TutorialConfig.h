2c3b25fc67bded7a75fe40e036eaa60237fc8680
Banjorabbit <kristiantimm@gmail.com>
2022-06-23 21:57:51 +0200

    removed file

--- inc/TutorialConfig.h
+++
@@ -1,6 +0,0 @@
-// the configured options and settings for Tutorial
-#define Tutorial_VERSION_MAJOR 0
-#define Tutorial_VERSION_MINOR 0
-#define Tutorial_VERSION_PATCH 1
-#define DOCTEST_CONFIG_DISABLE 
-/* #undef ENABLE_TESTS */
